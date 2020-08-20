601e99b078982f389268dc54d9037db61dd2f2a9
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-20 10:45:06 -0500

    adds function type setting example

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
-interface Person {
-  first: string,
-  last: string
+// tutorial: https://www.youtube.com/watch?v=ahCwqrYpIuM
+
+// this shows how to specify param types of number and a return type of string
+// can also set type to void
+function pow(x: number, y: number): string {
+  return Math.pow(x, y).toString();
 }
 
-const person: Person = {
-  first: 'Jedd',
-  last: 'Delaney'
-}
 
-const person2: Person = {
-  first: 'Usane',
-  last: 'Bult',
-  fast: true
-}
+// this shows that you can create an interface which can be used to enforce structure
+// interface Person {
+//   first: string,
+//   last: string
+// }
+
+// const person: Person = {
+//   first: 'Jedd',
+//   last: 'Delaney'
+// }
+
+// const person2: Person = {
+//   first: 'Usane',
+//   last: 'Bult',
+//   fast: true
+// }
