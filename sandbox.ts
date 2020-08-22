c3b336922f106181dcd42bf326c0f5a402e69e6a
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-22 12:01:23 -0500

    tsc tests complete

--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,11 @@
 const character = 'Nobo';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+  console.log(input);
+});
