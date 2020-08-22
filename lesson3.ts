0a79d2c7d3af77ad981720649941f62b5b9570fc
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-22 12:22:06 -0500

    lesson 3 complete

---
+++ lesson3.ts
@@ -0,0 +1,36 @@
+//---- lesson 3 -----
+
+// arrays
+let names = ['link', 'ganon', 'zelda']; // array of strings only
+
+names.push(2);
+names.push('tingle');
+names[0] = 1;
+names[0] = 'tingle';
+
+let mixed = ['ken', 4, 'chun-li', 8, true]; //mixed array
+
+mixed.push(2);
+mixed.push('tingle');
+mixed[0] = 1;
+mixed[0] = 'tingle';
+
+// objects
+let ninja = {
+  name: 'Utake', // each key behaves as a variable so the type cannot be changed
+  belt: 'black',
+  age: 42
+}
+
+ninja.age = 30; //this is fine because age is already a number
+ninja.age = '45'; //.age cannot be changed to a string
+
+ninja.skills = ['sneak', 'flying knee']//cant add additional properties after declaring the object
+
+// we can reassign the object but it has to match the structure of the original object
+ninja = {
+  name: 'newName',
+  belt: 'red',
+  age: 23,
+  skills: ['sneak', 'flying knee']
+}
--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,2 @@
 //tsc sandbox.ts -w to continually compile on saving
+// TUTORIAL: https://www.youtube.com/watch?v=157NopQ-chU&list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI&index=4
